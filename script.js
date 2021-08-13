7398302a9cc3e35f13c6ec293f19a80993e6ba15
nazif <ningis4u@gmail.com>
2021-08-13 11:05:26 +0100

    Error corrected

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
-function setclock() {
+function setClock() {
   const currentDate = new Date()
   const secondsRatio = currentDate.getSeconds() / 60
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
